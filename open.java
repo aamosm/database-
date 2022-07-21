e84f1f8f8e9881968ace52b2eeaac38f4f251d13
aamosm <109422037+aamosm@users.noreply.github.com>
2022-07-21 15:19:02 +0530

    Create open.java

---
+++ open.java
@@ -0,0 +1,63 @@
+import java.awt.Robot;
+import java.awt.AWTException;
+import java.awt.event.KeyEvent;
+
+public class open {
+    public static void main(String[] args) throws InterruptedException, AWTException {
+
+        Robot r = new Robot();
+
+        r.keyPress(KeyEvent.VK_WINDOWS);
+        r.keyRelease(KeyEvent.VK_WINDOWS);
+        Thread.sleep(500);
+        r.keyPress(KeyEvent.VK_C);
+        r.keyRelease(KeyEvent.VK_C);
+        Thread.sleep(500);
+        r.keyPress(KeyEvent.VK_H);
+        r.keyRelease(KeyEvent.VK_H);
+        Thread.sleep(500);
+        r.keyPress(KeyEvent.VK_R);
+        r.keyRelease(KeyEvent.VK_R);
+        Thread.sleep(500);
+        r.keyPress(KeyEvent.VK_O);
+        r.keyRelease(KeyEvent.VK_O);
+        Thread.sleep(500);
+        r.keyPress(KeyEvent.VK_M);
+        r.keyRelease(KeyEvent.VK_M);
+        Thread.sleep(500);
+        r.keyPress(KeyEvent.VK_E);
+        r.keyRelease(KeyEvent.VK_E);
+        Thread.sleep(500);
+        r.keyPress(KeyEvent.VK_ENTER);
+        r.keyRelease(KeyEvent.VK_ENTER);
+        Thread.sleep(500);
+        r.keyPress(KeyEvent.VK_A);
+        r.keyRelease(KeyEvent.VK_A);
+        Thread.sleep(500);
+r.keyPress(KeyEvent.VK_A);
+        r.keyRelease(KeyEvent.VK_A);
+        Thread.sleep(500);
+        r.keyPress(KeyEvent.VK_M);
+        r.keyRelease(KeyEvent.VK_M);
+        Thread.sleep(500);
+        r.keyPress(KeyEvent.VK_O);
+        r.keyRelease(KeyEvent.VK_O);
+        Thread.sleep(500);
+        r.keyPress(KeyEvent.VK_M);
+        r.keyRelease(KeyEvent.VK_M);
+Thread.sleep(500);
+        r.keyPress(KeyEvent.VK_OEM_PERIOD);
+        r.keyRelease(KeyEvent.VK_OEM_PERIOD);
+        Thread.sleep(500);
+        r.keyPress(KeyEvent.VK_G);
+        r.keyRelease(KeyEvent.VK_G);
+Thread.sleep(500);
+       r.keyPress(KeyEvent.VK_I);
+        r.keyRelease(KeyEvent.VK_I);
+Thread.sleep(500);
+        r.keyPress(KeyEvent.VK_ENTER);
+        r.keyRelease(KeyEvent.VK_ENTER);
+        Thread.sleep(500);
+
+    }}
+ 
